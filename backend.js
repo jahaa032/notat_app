a955a2d44d9050851f51a90323f8a249a72a11bb
thewiz <jfhannestad@gmail.com>
2026-05-21 11:25:41 +0200

    Fixed, change arrow function to function since we used this.lastID

--- backend.js
+++ backend.js
@@ -84,7 +84,7 @@ app.post("/notes", (req, res) => {
   db.run(
     "INSERT INTO Notes (Tittel, Body) VALUES (?, ?)",
     [Tittel, Body],
-    (err) => {
+    function (err) {
       if (err) return res.status(500).json({ error: err.message });
 
       res.json({ id: this.lastID, Tittel, Body });
